fee541ba48e6b3dd4a6a64dd45fdedd8fddb375d
giuseppe.filomena <F.pena2007>
2017-09-28 13:58:29 +0200

    routing Auth settled on all Components disabled breadcrumbs

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,64 +1,54 @@
-﻿import { Routes, RouterModule } from "@angular/router"
+﻿import { Routes, RouterModule } from '@angular/router';
 import { NgModule, ModuleWithProviders } from '@angular/core';
-
-import { HomeComponent } from "./home/index"
-import { LoginComponent } from "./login/index"
-import { RegisterComponent } from "./register/index"
-import { SearchFormComponent} from "./search/search-form.component"
-import { AuthGuard } from "./_guards/index"
-import { SettingComponent } from "./setting/index"
-import { BookmarksComponent } from "./bookmarks/index"
-import { SearchHistoryComponent } from "./search-history/index"
-import { DialogRepositoryComponent } from "./dialog-repository/index"
-import {HashLocationStrategy, Location, LocationStrategy, APP_BASE_HREF} from '@angular/common';
-import { ProfileComponent } from "./profile/index"
+import { HomeComponent } from './home/index';
+import { LoginComponent } from './login/index';
+import { RegisterComponent } from './register/index';
+import { SearchFormComponent } from './search/search-form.component';
+import { AuthGuard } from './_guards/index';
+import { SettingComponent } from './setting/index';
+import { BookmarksComponent } from './bookmarks/index';
+import { SearchHistoryComponent } from './search-history/index';
+import { DialogRepositoryComponent } from './dialog-repository/index';
+import { HashLocationStrategy, Location, LocationStrategy, APP_BASE_HREF } from '@angular/common';
+import { ProfileComponent } from './profile/index';
 const appRoutes: Routes = [
-{ path: "",
-component: SearchFormComponent,
-canActivate: [AuthGuard],
-data: { breadcrumb: 'home' }
-/* children: [
-{ path: '', redirectTo: 'bookmarks', pathMatch: 'full' },
-{
-path: "bookmarks",
-component: BookmarksComponent,
-data: { breadcrumb: 'bookmarks' }
-},
-{
-path: "setting",
-component: SettingComponent,
-data: { breadcrumb: 'setting' }
-}
-
-]*/
-},
-{ path: "login", component: LoginComponent , data: { breadcrumb: 'login' }},
-{ path: "register", component: RegisterComponent, data: { breadcrumb: 'register' }},
-{ path: "setting", component: SettingComponent , data: { breadcrumb: 'setting' }},
-{ path: "bookmarks", component: BookmarksComponent , data: { breadcrumb: 'bookmarks' }},
-{ path: "search-history", component: SearchHistoryComponent , data: { breadcrumb: 'search history' }},
-{ path: "setting-rep", component: DialogRepositoryComponent , data: { breadcrumb: 'setting repository' }},
-{ path: "profile", component: ProfileComponent , data: { breadcrumb: 'profile' },
-/*children: [
-{ path: '', redirectTo: 'profile', pathMatch: 'full' },
-{
-path: "setting",
-component: SettingComponent,
-data: { breadcrumb: 'setting' }
-} ]*/
-},
-
-// otherwise redirect to home
-{ path: "**", redirectTo: "" }
+    /*{ path: '',
+    component: SearchFormComponent,
+    canActivate: [AuthGuard],
+    data: { breadcrumb: 'home' }
+     children: [
+    { path: '', redirectTo: 'bookmarks', pathMatch: 'full' },
+    {
+    path: 'bookmarks',
+    component: BookmarksComponent,
+    data: { breadcrumb: 'bookmarks' }
+    },
+    {
+    path: 'setting',
+    component: SettingComponent,
+    data: { breadcrumb: 'setting' }
+    }
+    ]
+    },*/
+    { path: 'home', component: SearchFormComponent, canActivate: [AuthGuard], data: { breadcrumb: 'home' } },
+    { path: 'login', component: LoginComponent, data: { breadcrumb: 'login' } },
+    { path: 'register', component: RegisterComponent, data: { breadcrumb: 'register' } },
+    { path: 'setting', component: SettingComponent, canActivate: [AuthGuard], data: { breadcrumb: 'setting' } },
+    { path: 'bookmarks', component: BookmarksComponent, canActivate: [AuthGuard], data: { breadcrumb: 'bookmarks' } },
+    { path: 'search-history', component: SearchHistoryComponent, canActivate: [AuthGuard], data: { breadcrumb: 'search history' } },
+    { path: 'setting-rep', component: DialogRepositoryComponent, canActivate: [AuthGuard], data: { breadcrumb: 'setting repository' } },
+    { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard], data: { breadcrumb: 'profile' } },
+    // otherwise redirect to home
+    { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ]
 
 @NgModule({
-imports: [ RouterModule.forRoot(appRoutes)  ],
-exports: [ RouterModule ],
-providers: [
-{ provide: LocationStrategy, useClass: HashLocationStrategy },
-{ provide: APP_BASE_HREF, useValue: '/' } // <--- this right here
-]
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule],
+    providers: [
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
+        { provide: APP_BASE_HREF, useValue: '/' } // <--- this right here
+    ]
 })
 
-export class AppRoutingModule {}
+export class AppRoutingModule { }
