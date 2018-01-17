8a4919a4fd5c0994a1f89c26ab7aa6730d61fc95
Pasquale Panuccio <pasquale.panuccio@finconsgroup.com>
2018-01-17 18:00:28 +0100

    updated environment.dev

--- ui/src/environments/environment.dev.ts
+++ ui/src/environments/environment.dev.ts
@@ -1,9 +1,9 @@
 export const environment = {
-    production: true,
+    production: false,
     serviceUrl: 'http://localhost:9000/api/v1',
     auth_clientID: 'XYQEF1J7DXFJYD2gOkpjz5ZsuNAbZ0o7',
     auth_domain: 'pasquydomain.eu.auth0.com',
     auth_apiUrl: 'https://pasquydomain.eu.auth0.com/api/v2/',
-    auth_callbackURL: 'http://localhost:9000/',
+    auth_callbackURL: 'http://localhost:4200/',
     vimeo_clientID: '81300b8b6457c4c8da7935e2cbee238f2a756cd5'
   };
