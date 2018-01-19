f22bf31380fc2f3782ce085ae20229ad28f3c3be
giuseppe.filomena <F.pena2007>
2018-01-19 15:48:46 +0100

    dev - switched port to 4200 hidden Register button on login page

--- ui/src/environments/environment.dev.ts
+++ ui/src/environments/environment.dev.ts
@@ -4,6 +4,6 @@ export const environment = {
     auth_clientID: 'XYQEF1J7DXFJYD2gOkpjz5ZsuNAbZ0o7',
     auth_domain: 'pasquydomain.eu.auth0.com',
     auth_apiUrl: 'https://pasquydomain.eu.auth0.com/api/v2/',
-    auth_callbackURL: 'http://localhost:9000/',
+    auth_callbackURL: 'http://localhost:4200/',
     vimeo_clientID: '81300b8b6457c4c8da7935e2cbee238f2a756cd5'
   };
