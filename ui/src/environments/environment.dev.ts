08d20ddf956cc2c5c2c8be7bc659524f6fe0e829
giuseppe.filomena <F.pena2007>
2018-01-19 15:52:54 +0100

    environment.dev - production:false

--- ui/src/environments/environment.dev.ts
+++ ui/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: true,
+    production: false,
     serviceUrl: 'http://localhost:9000/api/v1',
     auth_clientID: 'XYQEF1J7DXFJYD2gOkpjz5ZsuNAbZ0o7',
     auth_domain: 'pasquydomain.eu.auth0.com',
