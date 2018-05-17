b310f7dc02509b002c6df5d26e1bf1652f67e76f
Pasquale Panuccio <pasquale.panuccio@finconsgroup.com>
2018-05-17 11:45:40 +0200

    updated port

--- ui/src/environments/environment.dev.ts
+++ ui/src/environments/environment.dev.ts
@@ -4,8 +4,8 @@ export const environment = {
     auth_clientID: 'chMO60cB8YoeG0PCSisJ6WZA73WOaya7',
     auth_domain: 'producer-account.eu.auth0.com',
     auth_apiUrl: 'https://producer-account.eu.auth0.com/api/v2/',
-    auth_callbackURL: 'http://localhost:4200/callback',
+    auth_callbackURL: 'http://localhost:9000/callback',
     vimeo_clientID: '81300b8b6457c4c8da7935e2cbee238f2a756cd5',
     vimeo_client_secret: 'T7olulWMgGe0C73zPL63ALBckCsQ6ZoEreNcwoLcCPU0HyQjFEiEtLhvE2pi9iqpNFwjRwr7+wZGiT1QtoBIHBBoqHe/LevVt1PByXZYiOydM5CzmSkmkRMu+MWIcb1o',
-    auth_logoutUrl: 'http://localhost:4200/'
+    auth_logoutUrl: 'http://localhost:9000/'
   };
