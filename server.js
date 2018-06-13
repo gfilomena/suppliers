b16d272b1ebd0ed44f96f7638729fbec9a293466
Giuseppe Filomena <salsx@hotmail.it>
2018-06-14 00:17:26 +0200

    new fix heroku

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express'),
 	path = require('path'),
 	bodyParser = require('body-parser'),
 	cors = require('cors'),
-	mongoose = require('mongoose'),
-	config = require('./config/DB');
+	mongoose = require('mongoose');
+
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/suppliers';
+
 
 const supplierRoutes = require('./routes/supplier.route');
 
 	mongoose.Promise = global.Promise;
-	mongoose.connect(config.DB).then(
+	mongoose.connect(uri).then(
 	  () => {console.log('Database is connected') },
 	  err => { console.log('Can not connect to the database'+ err)}
 	);
